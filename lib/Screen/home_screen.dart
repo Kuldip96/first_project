4b1c5df8507e50649d913ac28bb3571e8d1457fb
karan-77 <106381064+karan-77@users.noreply.github.com>
2023-10-19 18:17:12 +0530

    first commit

---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class HomeScreen1 extends StatelessWidget {
+  const HomeScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
